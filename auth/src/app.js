c78b5cebc02390296eecdc235596a7fc82cc86cd
Ngọc Diễm Phạm <ndiem@nDiem-MacBook-Pro.local>
2025-10-22 22:47:41 +0700

    test thu them lan 12.3

--- auth/src/app.js
+++ auth/src/app.js
@@ -14,15 +14,11 @@ class App {
   }
 
   async connectDB() {
-    // await mongoose.connect(config.mongoURI, {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    // });
-    // console.log("MongoDB connected");
-   await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch(err => console.error("❌ MongoDB connection error:", err));
-
+    await mongoose.connect(config.mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected");
   }
   
 
